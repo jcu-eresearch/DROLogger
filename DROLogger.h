c74e8ad9492b001ae30cdc5d5383687ec7998f5a
NigelB <nigel.blair@gmail.com>
2014-08-30 00:37:15 +1000

    Add DROLogger files

---
+++ DROLogger.h
@@ -0,0 +1,30 @@
+/*
+	Daintree Rainforest Observatory Data Logger
+	Copyright (C) 2014  NigelB
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+*/
+
+#ifndef _DROLogger_H_
+#define _DROLogger_H_
+#include "Arduino.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+#ifdef __cplusplus
+}
+#endif
+
+#endif
