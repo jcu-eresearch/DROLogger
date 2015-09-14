716b7a586fbdaf546badd2344b5b52e473036f83
nigelb <nigelb@users.noreply.github.com>
2015-09-14 16:12:03 +1000

    Add Arduino Time Git Repository

--- bin/getLibs.sh
+++ bin/getLibs.sh
@@ -10,13 +10,14 @@ pushd $lib_dir
 git clone https://github.com/Tecsmith/DS3232RTC.git
 git clone https://github.com/milesburton/Arduino-Temperature-Control-Library.git
 git clone https://github.com/nigelb/Arduino-DS2762.git
+git clone https://github.com/PaulStoffregen/Time.git
 
 rm .onewire.zip
 rm -Rf OneWire
 wget http://www.pjrc.com/teensy/arduino_libraries/OneWire.zip -O .onewire.zip
 unzip .onewire.zip
 
-rm .time.zip
-rm -Rf Time
-wget http://www.pjrc.com/teensy/arduino_libraries/Time.zip -O .time.zip
-unzip .time.zip
+#rm .time.zip
+#rm -Rf Time
+#wget http://www.pjrc.com/teensy/arduino_libraries/Time.zip -O .time.zip
+#unzip .time.zip
